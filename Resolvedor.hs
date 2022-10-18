b30cad12a688442fdcd06be150f5e35af0cc96e9
isabellaaquino <isabellaviviani6@gmail.com>
2022-10-17 23:41:40 -0300

    choices function update

--- Resolvedor.hs
+++ Resolvedor.hs
@@ -1,42 +1,86 @@
 module Resolvedor where
+
+import Data.List
 import Tabuleiro
 
-type Choices = [Value]
+type Escolhas = [Valor]
+
 --o tabuleiro será válido se:
 --1: Nenhuma célula vizinha (horizontal e vertical) possuir os mesmos valores;
 --2: A ordem vertical dos valores de uma região seja decrescente.
-valido :: GradeValores -> GradeRegioes -> Bool 
+valido :: GradeValores -> GradeRegioes -> Bool
 valido valores regioes = all vizinhosDiferentes (linhas valores) && all vizinhosDiferentes (colunas valores) && all ordemDecrescente (colunas (matrizValoresRegioes valores regioes))
 
 --verifica se os vizinhos possuem valores diferentes (incompleta ?)
-vizinhosDiferentes :: Eq a => [a] -> Bool 
+vizinhosDiferentes :: Eq a => [a] -> Bool
 vizinhosDiferentes [] = True
 vizinhosDiferentes [_] = True
-vizinhosDiferentes (x:xs)
-    | (x /= head xs) = vizinhosDiferentes xs 
-    | otherwise = False
-  
+vizinhosDiferentes (x : xs)
+  | (x /= head xs) = vizinhosDiferentes xs
+  | otherwise = False
+
 --verifica se a região segue a ordem decrescente na vertical (incompleta, falar com o professor)
-ordemDecrescente :: [a] -> Bool 
-ordemDecrescente [] = True 
+ordemDecrescente :: Ord a => [a] -> Bool
+ordemDecrescente [] = True
 ordemDecrescente [_] = True
-ordemDecrescente (x:xs)
-    | (x > head xs) = ordemDecrescente xs 
-    | otherwise = False
+ordemDecrescente (x : xs)
+  | (x > head xs) = ordemDecrescente xs
+  | otherwise = False
 
 -- ____________________________________________________________________________________________________________________________________________________________________________________________________________________
 
-choices :: [(Int, Char)] -> Matrix Choices
-choices = map (map choices)
-    where
-        choice v = if (obterValorEscolhido v) == 0 then values (obterRegiaoEscolhida v) else [obterValorEscolhido v] 
+escolhas :: GradeValores -> GradeRegioes -> Matriz Escolhas
+escolhas valores regioes = map (map escolha) (zipWith zip valores regioes)
+  where
+    escolha (v, r) = if v == 0 then values (obterTamanhoRegiao r regioes) `minus` (obterValoresRegiao r valores regioes) else [v]
+
+-- [[1,3,5],[1,3,6],[1,4,5],[1,4,6],[2,3,5],[2,3,6],[2,4,5],[2,4,6]]
+
+teste :: GradeValores -> GradeRegioes -> Matriz Escolhas
+teste valores regioes = map (map choice) (zipWith zip valores regioes)
+  where
+    choice (v, p) = if v == 0 then [1 .. (obterTamanhoRegiao p regioes)] `minus` (obterValoresRegiao p valores regioes) else [v]
+
+minus :: Escolhas -> Escolhas -> Escolhas
+xs `minus` ys = if unica xs then xs else xs \\ ys
+
+-- Recebe uma matriz de escolhas e a matriz de posições.
+-- Invoca a função reduzEscolhasLista para cada coluna dividida por grupos.
+-- Retorna matriz de escolhas com escolhas reduzidas.
+-- reduzEscolhasPorGrupo :: Matriz Escolhas -> Tabuleiro -> Matriz Escolhas
+-- reduzEscolhasPorGrupo valores grupos = colunas $ colunasOrigem (map reduzEscolhasLista (gruposPorColuna valores grupos)) (tamanho valores)
+
+-- -- Reduz as escolhas dos valores da lista com base em elementos unitários
+-- reduzEscolhasLista :: Linha Escolhas -> Linha Escolhas
+-- reduzEscolhasLista xss = [xs `minus` singles | xs <- xss]
+--     where singles = concat (filter umElemento xss)
 
 cp :: [[a]] -> [[a]]
 cp [] = [[]]
-cp (xs:xss) = [y:ys | y <- xs, ys <- cp xss]
+cp (xs : xss) = [y : ys | y <- xs, ys <- cp xss]
 
-collapse :: Matrix [a] -> [Matrix a]
-collapse = cp . map cp
+--Propriedades da matriz de escolhas
 
-solve :: GradeValores -> GradeRegioes -> [GradeValores]
-solve valores regioes = filter (valido (collapse (choices)), regioes) -- TODO - passar parametro choices + testar
+--verifica se há somente uma escolha para a célula
+unica :: [a] -> Bool
+unica [_] = True
+unica _ = False
+
+--A matriz será 'completa' se todas as células possuírem apenas uma escolha
+completa :: Matriz Escolhas -> Bool
+completa = all (all unica)
+
+--A matriz será 'inválida' se alguma das células não possuir escolhas
+invalida :: Matriz Escolhas -> Bool
+invalida = any (any null)
+
+integrar :: Matriz [a] -> [Matriz a]
+integrar = cp . map cp
+
+-- solve :: GradeValores -> GradeRegioes -> [GradeValores]
+-- solve valores regioes = filter (valido (integrar (escolhas)), regioes) -- TODO - passar parametro choices + testar
+
+-- > choices               :: Grid -> Matrix Choices
+-- > choices               =  map (map choice)
+-- >                          where
+-- >                             choice v = if empty v then values else [v]
--- Tabuleiro.hs
+++ Tabuleiro.hs
@@ -5,26 +5,33 @@ import Data.List
 
 --definição das estruturas usadas para modelar o tabuleiro
 type GradeValores = Matriz Int
-type GradeRegioes = Matriz Char
-type Matriz a = [Linha a]
-type Linha a = [a]
-type Value = Int
 
-values :: a -> [Value]
-value a = [1..obterTamanhoRegiao a]
+type GradeRegioes = Matriz Char
+
+type Matriz a = [Linha a]
+
+type Linha a = [a]
+
+type Valor = Int
+
+values :: Int -> [Int]
+values a = [1 .. a]
 
 --cria uma lista de tuplas correspondentes ao valor e à região
 --de cada célula
 listaValoresRegioes :: GradeValores -> GradeRegioes -> [(Int, Char)]
-listaValoresRegioes valores regioes = foldl1' (++) (matrizValoresRegioes valores regioes)
+listaValoresRegioes valores regioes = foldl1 (++) (matrizValoresRegioes valores regioes)
+
+listaRegioes :: GradeRegioes -> [Char]
+listaRegioes regioes = foldl1 (++) (regioes)
 
 --cria uma matriz única, juntando valores e regiões em tuplas
 matrizValoresRegioes :: GradeValores -> GradeRegioes -> Matriz (Int, Char)
-matrizValoresRegioes= zipWith zip
+matrizValoresRegioes = zipWith zip
 
 --obtém as linhas da matriz
 linhas :: Matriz a -> [Linha a]
-linhas = id 
+linhas = id
 
 --obtém valor da regiao escolhida
 obterRegiaoEscolhida :: (Int, Char) -> Char
@@ -39,13 +46,36 @@ colunas :: Matriz a -> [Linha a]
 colunas = transpose
 
 --verifica se a célula está vazia
-celulaVazia :: Int -> Bool 
+celulaVazia :: Int -> Bool
 celulaVazia = (== 0)
 
+ -- [['a','a','b','b','c','d','e','e'],
+ -- ['a','a','g','b','f','d','d','e'],  
+ -- ['g','g','g','h','f','i','j','j'],
+ -- ['l','l','l','h','f','i','i','j'],
+ -- ['m','h','h','h','h','i','i','j'],
+ -- ['m','n','o','o','o','p','k','j'],
+ -- ['n','n','n','n','r','p','p','p'],
+ -- ['s','r','r','r','r','p','q','q']]
+
+
+-- Recebe o número de um bloco e a matriz de posições.
+-- Retorna o tamanho do bloco correspondente.
+tamanhoGrupo :: Eq m => m -> Matriz m -> Int
+tamanhoGrupo _ [] = 0
+tamanhoGrupo id grupos = sum [count id p | p <- grupos]
+  where count x xs = length (filter (==x) xs)
+
 --obtém o tamanho de uma região para usar como limite de escolha
-obterTamanhoRegiao :: Char -> GradeValores -> GradeRegioes -> Int
-obterTamanhoRegiao x valores regioes = length (filter ((==x) . snd) (listaValoresRegioes valores regioes))
+obterTamanhoRegiao :: Char -> GradeRegioes -> Int
+obterTamanhoRegiao x regioes = length (filter (==x) (listaRegioes regioes)) 
 
 --obtém valores que já foram usados na região
 obterValoresRegiao :: Char -> GradeValores -> GradeRegioes -> [Int]
-obterValoresRegiao x valores regioes = map fst (filter ((==x) . snd) (listaValoresRegioes valores regioes))
+obterValoresRegiao x valores regioes = map fst (filter ((== x) . snd) (listaValoresRegioes valores regioes))
+
+verificaRegioesNVezes 0 = return ()
+verificaRegioesNVezes n =
+ do
+  
+  verificaRegioesNVezes (n-1)
